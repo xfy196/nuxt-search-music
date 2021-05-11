cb7b1921ab588b3b42e65539c5da9a75160f2812
xfy196 <xufuying@outlook.com>
2021-05-11 13:38:12 +0800

    init nuxt-search-music

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'search-music',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/element-ui',
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios', '@nuxtjs/proxy'
+  ],
+  axios: {
+    retry: { retries: 3 },
+    //开发模式下开启debug
+    debug: process.env._ENV == "production" ? false : true,
+    //设置不同环境的请求地址
+    withCredentials: true,
+    headers: { 'Content-Type': 'application/json', 'crossDomain': true },
+    timeout: 5000,
+  },
+  proxy: {
+    '/api/': {
+      target: 'https://musicapi.xxytime.top',
+      pathRewrite: {
+        // '^/api/': ''
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/^element-ui/],
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <el-container class="container">
+    <el-header class="header">
+      <el-input
+        placeholder="请输入歌曲"
+        v-model="seachVal"
+        class="search"
+        @change="handleClick"
+      >
+        <el-button slot="append" icon="el-icon-search"
+        v-loading.fullscreen.lock="loading"
+          @click="handleClick"
+        ></el-button>
+      </el-input>
+    </el-header>
+    <el-main class="main">
+      <ul v-infinite-scroll="load" infinite-scroll-delay="300" infinite-scroll-distance="0" :infinite-scroll-disabled="!disabled">
+        <li v-for="item in result.songs" :key="item.id">{{item.name}} -- {{item.ar[0].name}}</li>
+      </ul>
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts">
+import axios from "axios"
+export default {
+  data(){
+    return {
+      seachVal: "",
+      result: {
+        hasMore: true,
+        songCount: 0,
+        songs:[]
+      },
+      page: 0,
+      pageNum: 80,
+      loading: false,
+    }
+  },
+  computed: {
+    disabled(){
+      return this.loading || this.result.hasMore
+    }
+  },
+  methods: {
+    handleClick(){
+      this.seachVal = this.seachVal.trim()
+      this.page = 0
+      this.result.songCount = 0
+      this.result.songs = []
+      this.load()
+    },
+    async load(): Promise<void>{
+      if(!this.seachVal.trim()){
+        return;
+      }
+      this.loading = true
+     let result: any = await axios({
+        url: "/api/cloudsearch",
+        method: "GET",
+        params: {
+          keywords: this.seachVal,
+          limit: this.pageNum * ++this.page
+        }
+      })
+      this.loading = false
+      if(result.data.code == 200){
+        this.result = {
+          ...this.result,
+          ...result.data.result
+        }
+        console.log(this.result)
+      }else if(result.data.code == 400){
+        this.result.hasMore = false
+        this.$message({
+          message: "没有更多歌曲了",
+          type: "info"
+        })
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.container{
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+.header {
+  position: relative;
+}
+.search{
+  width: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+}
+.main {
+  width: 72%;
+  margin: 0 auto;
+}
+ul,li {
+  list-style: none;
+}
+</style>
